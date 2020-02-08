63ce0e2e7cda22f09bb9e4e227004702e0c8b655
ChrisQian <416734061@qq.com>
2020-02-09 02:22:12 +0800

    merge sort

---
+++ src/sort/MergeSort.java
@@ -0,0 +1,53 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * 时间复杂度：O(nlogn)
+ * 空间复杂度：O(N)，归并排序需要一个与原数组相同长度的数组做辅助来排序
+ */
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22 };
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void mergeSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    public static void sort(int[] arr, int left, int right) {
+        if(left == right) {
+            return;
+        }
+        int mid = left + ((right - left) >> 1);
+        sort(arr, left, mid);
+        sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = 0;
+        int p1 = left;
+        int p2 = mid + 1;
+        // 比较左右两部分的元素，哪个小，把那个元素填入temp中
+        while(p1 <= mid && p2 <= right) {
+            temp[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        // 上面的循环退出后，把剩余的元素依次填入到temp中
+        // 以下两个while只有一个会执行
+        while(p1 <= mid) {
+            temp[i++] = arr[p1++];
+        }
+        while(p2 <= right) {
+            temp[i++] = arr[p2++];
+        }
+        // 把最终的排序的结果复制给原数组
+        for(i = 0; i < temp.length; i++) {
+            arr[left + i] = temp[i];
+        }
+    }
+}
