93a1993789962df7c8278ac74bf61500dd6ce39c
ChrisQian <416734061@qq.com>
2020-02-08 18:38:58 +0800

    insert sort

---
+++ src/sort/InsertSort.java
@@ -0,0 +1,41 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+
+    public static void main(String[] args) {
+        int[] array = {3, 1, -1, 10, 2};
+//        simpleInsertSort(array);
+        optimalInsertSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void simpleInsertSort(int[] array){
+        for (int i = 1; i < array.length; i++) {
+
+            int insertValue = array[i];
+            int insertIndex = i - 1;
+            while (insertIndex >= 0 && insertValue < array[insertIndex]) {
+                array[insertIndex + 1] = array[insertIndex];//将array[insertIndex]后移
+                insertIndex --;
+            }
+            array[insertIndex + 1] = insertValue;
+        }
+    }
+
+    public static void optimalInsertSort(int[] array){
+        for (int i = 1; i < array.length; i++) {
+
+            int insertValue = array[i];
+            int insertIndex = i - 1;
+            while (insertIndex >= 0 && insertValue < array[insertIndex]) {
+                array[insertIndex + 1] = array[insertIndex];//将array[insertIndex]后移
+                insertIndex --;
+            }
+            if (insertIndex + 1 != i){
+                array[insertIndex + 1] = insertValue;
+            }
+        }
+    }
+}
